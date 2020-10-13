b391dc462e74bb13f7c5b160883128658c16fd99
daeunSong <daeun7250@gmail.com>
2020-10-14 00:10:21 +0900

    Fix for orinetation

--- demo/exp.py
+++ demo/exp.py
@@ -1,5 +1,5 @@
 # ~ from hpp.corbaserver.rbprm.talos_abstract import Robot
-from talos_rbprm.talos_abstract import Robot 
+from talos_rbprm.talos_abstract import Robot
 from hpp.corbaserver.problem_solver import ProblemSolver
 from hpp.gepetto import Viewer
 from hpp.corbaserver import Client
@@ -11,7 +11,7 @@ Robot.urdfName += "_large"
 
 packageName = 'hpp_environments'
 meshPackageName = 'hpp_environments'
-I = 0
+I = 2
 TEST = True
 GUIDE = True
 CONTINUOUS = False
@@ -30,7 +30,7 @@ if not GUIDE:
     step_num = step_nums[I]
 else:
     if INTERSECT:
-        fileName += "_Wi" 
+        fileName += "_Wi"
     else: fileName += "_Ww"
     if CONTINUOUS:
         fileName += "_c"
@@ -70,7 +70,7 @@ if pbName == 'playground':
     # q_init[0:3]=p_start
     # q_goal[0:3]=p_goal
     # v(q_init)
-    
+
     x_min = min([set[0][0][0] for set in LIST_LINKS]+[set[1][0][0] for set in LIST_LINKS])
     x_max = max([set[0][0][1] for set in LIST_LINKS]+[set[1][0][1] for set in LIST_LINKS])
     y_min = min([set[0][1][0] for set in LIST_LINKS]+[set[1][1][0] for set in LIST_LINKS])
@@ -117,9 +117,9 @@ rbprmBuilder.client.robot.setExtraConfigSpaceBounds(extraDofBounds)
 indexECS = rbprmBuilder.getConfigSize() - rbprmBuilder.client.robot.getDimensionExtraConfigSpace()
 
 
-# Creating an instance of HPP problem solver 
+# Creating an instance of HPP problem solver
 ps = ProblemSolver( rbprmBuilder )
-# define parameters used by various methods : 
+# define parameters used by various methods :
 ps.setParameter("Kinodynamic/velocityBound",vMax)
 ps.setParameter("Kinodynamic/accelerationBound",aMax)
 ps.setParameter("Kinodynamic/forceYawOrientation",True)
@@ -130,14 +130,14 @@ ps.setParameter("DynamicPlanner/sizeFootY",0.12)
 ps.setParameter("DynamicPlanner/friction",mu)
 if pbName in ['stairs_2', 'rubbles_stairs_1', 'playground']:
     step_size = 1.0
-    ps.setParameter("Kinodynamic/velocityBound",0.3) 
+    ps.setParameter("Kinodynamic/velocityBound",0.3)
     ps.setParameter("Kinodynamic/accelerationBound",0.1)
 elif pbName == 'debris':
     step_size = 0.9
-    ps.setParameter("Kinodynamic/velocityBound",0.4) 
+    ps.setParameter("Kinodynamic/velocityBound",0.4)
     ps.setParameter("Kinodynamic/accelerationBound",0.1)
 if pbName == 'rubbles_stairs_1':
-    ps.setParameter("Kinodynamic/velocityBound",0.25) 
+    ps.setParameter("Kinodynamic/velocityBound",0.25)
 # sample only configuration with null velocity and acceleration :
 ps.setParameter("ConfigurationShooter/sampleExtraDOF",False)
 ps.setParameter("PathOptimization/RandomShortcut/NumberOfLoops",100)
@@ -155,7 +155,7 @@ afftool.setAffordanceConfig('Support', [0.5, 0.03, 0.00005])
 if pbName == 'ground':
     afftool.loadObstacleModel (packageName, "multicontact/ground", "planning", vf) # flat ground
 elif pbName == 'stairs' or pbName == 'debris':
-    # ~ afftool.loadObstacleModel (packageName, "multicontact/daeun/"+pbName, "planning", vf,reduceSizes=[0.015,0.,0.])    
+    # ~ afftool.loadObstacleModel (packageName, "multicontact/daeun/"+pbName, "planning", vf,reduceSizes=[0.015,0.,0.])
     afftool.loadObstacleModel ("package://hpp_environments/urdf/multicontact/daeun/"+pbName+".urdf", "planning", vf,reduceSizes=[0.015,0.,0.])
 else:
     afftool.loadObstacleModel ("package://hpp_environments/urdf/multicontact/daeun/"+pbName+".urdf", "planning", vf)
@@ -179,12 +179,12 @@ v.addLandmark(v.sceneName,1)
 #     q_goal = q_init [::]
 #     q_goal[0:3] = [1.87,0.9,1.58]
 # else:
-#     q_init [0:3] = p_start 
+#     q_init [0:3] = p_start
 #     q_goal = q_init [::]
-#     q_goal [0:3] = p_goal 
+#     q_goal [0:3] = p_goal
 #     q_goal[-6:-3] = [0,0,0]
 
-    
+
 # ps.setInitialConfig (q_init)
 # ps.addGoalConfig (q_goal)
 # v(q_goal)
@@ -206,7 +206,7 @@ ps.selectPathPlanner("DynamicPlanner")
 #     ps.optimizePath(ps.numberPaths()-1)
 # pathId = ps.numberPaths() -1
 
-# # display solution : 
+# # display solution :
 # from hpp.gepetto import PathPlayer
 # pp = PathPlayer (v)
 # pp.dt=0.1
@@ -229,16 +229,16 @@ from sl1m.problem_definition import *
 from sl1m.planner_scenarios.talos.constraints import *
 from numpy import array, asmatrix, matrix, ones
 
-# generate problem 
+# generate problem
 def gen_pb(init, c0, R, surfaces):
-    
+
     nphases = len(surfaces)
     res = { "p0" : init, "c0" : c0, "nphases": nphases}
-    
+
     # phaseData = [ {"moving" : i%2, "fixed" : (i+1) % 2 , "K" : [copyKin(kinematicConstraints) for _ in range(len(surfaces[i]))], "relativeK" : [relativeConstraints[(i)%2] for _ in range(len(surfaces[i]))], "S" : surfaces[i] } for i in range(nphases)]
     phaseData = [ {"moving" : i%2, "fixed" : (i+1) % 2 , "K" : [genKinematicConstraints(left_foot_constraints,right_foot_constraints,index = i, rotation = R, min_height = 0.3) for _ in range(len(surfaces[i]))], "relativeK" : [genFootRelativeConstraints(right_foot_in_lf_frame_constraints,left_foot_in_rf_frame_constraints,index = i, rotation = R)[(i) % 2] for _ in range(len(surfaces[i]))], "rootOrientation" : R[i], "S" : surfaces[i] } for i in range(nphases)]
     res ["phaseData"] = phaseData
-    return res 
+    return res
 
 def footPosFromCOM(init_com):
     lf_0 = array(init_com[0:3]) + array([0, 0.085,-0.98])
@@ -261,23 +261,24 @@ def plotSurface (points, ax, plt,color_id = -1):
     colors = ['r','g','b','m','y','c']
     if color_id == -1: ax.plot(xs,ys,zs)
     else: ax.plot(xs,ys,zs, color = colors[color_id%6])
-        
+
 def draw_scene(surfaces, ax = None):
     colors = ['r','g','b','m','y','c']
     color_id = 0
-    if ax is None:        
+    if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
     # [draw_rectangle(l,ax) for l in all_surfaces]
-    for i, surfaces_phase in enumerate(surfaces): 
+    for i, surfaces_phase in enumerate(surfaces):
         for surface in surfaces_phase:
             plotSurface(surface, ax, plt, color_id)
         color_id += 1
-        
+
     plt.ion()
     plt.show()
-    
-    return ax    
+    plt.pause(2)
+
+    return ax
 
 
 import pickle
@@ -291,7 +292,7 @@ def readFromFile (fileName):
             line = pickle.load(f)
           except EOFError:
             break
-          data.append(line)  
+          data.append(line)
   except:
       return None
   return data[0]
@@ -324,7 +325,7 @@ if data != None:
     fail1 = data[4]
     fail2 = data[5]
     failcase = data[6]
-else :    
+else :
     phase_num = []
     candidate_num = []
     mip_comp_gr = []
@@ -336,9 +337,9 @@ else :
 
 
 from sl1m.fix_sparsity import solveMIP, solveL1
-from random import *   
+from random import *
 
-while run < MAX_RUN :   
+while run < MAX_RUN :
     q_init = rbprmBuilder.getCurrentConfig ()
     ### path planning
     if pbName == 'ground':
@@ -359,11 +360,11 @@ while run < MAX_RUN :
         p_goal = [1.8,0.8,0.98]
         q_goal[0:3] = p_goal
     else:
-        q_init [0:3] = p_start 
+        q_init [0:3] = p_start
         q_goal = q_init [::]
-        q_goal [0:3] = p_goal 
+        q_goal [0:3] = p_goal
         q_goal[-6:-3] = [0,0,0]
-        
+
     ps.setInitialConfig (q_init)
     ps.addGoalConfig (q_goal)
 
@@ -376,13 +377,13 @@ while run < MAX_RUN :
     pathId = ps.numberPaths() -1
     print ("done optimizing.")
 
-    
+
     ### contact planning
     print ("############### run : ", run+1)
 
     s_p0 = ps.getInitialConfig()[0:3]; init = footPosFromCOM(s_p0)
     g_p0 = ps.getGoalConfigs()[0][0:3]; goal = footPosFromCOM(g_p0)
-    
+
     ################################ MIP ################################
     ### generate contact candidates
     if GUIDE:
@@ -397,9 +398,9 @@ while run < MAX_RUN :
     pb = gen_pb(init, s_p0, R, surfaces); phase = len(pb["phaseData"])
     ### solve
     res_MI = solveMIP(pb, surfaces, draw_scene, PLOT, CPP)
-    
+
     print(res_MI)
-    
+
     if not res_MI.success: # MIP FAIL
         continue
     else:
@@ -438,7 +439,7 @@ while run < MAX_RUN :
     #     if not res_MI_validation.success: # SL1M UNFEASIBLE
     #         fail2 += 1
     #     else:
-    #         sl1m_comp_gr += [res_L1.time] 
+    #         sl1m_comp_gr += [res_L1.time]
     #         # if TEST:
     #         #     with open("rubbles_stairs_res_cool",'wb') as f:
     #         #         pickle.dump(res_L1.res, f)
@@ -451,23 +452,23 @@ while run < MAX_RUN :
     #     # pos = []
     #     # for i in range(len(allfeetpos[0])):
     #     #     pos += [[allfeetpos[0][i],allfeetpos[1][i]]]
-    #     del pb["phaseData"][0]; #del pos[0]; 
+    #     del pb["phaseData"][0]; #del pos[0];
     #     # allfeetpos = pos
     #     from sl1m.constants_and_tools import replace_surfaces_with_ineq_in_problem
     #     replace_surfaces_with_ineq_in_problem(pb)
     #     cs = build_cs_from_sl1m_mip(pb,allfeetpos,rbprmBuilder,q_init)
     #     cs.saveAsBinary("sl1m_data/bridge.cs")
-    
+
     total_candidate = 0
     for surfs in surfaces:
         total_candidate += len(surfs)
-    
+
     phase_num += [phase]
     candidate_num += [float(total_candidate-2)/float(len(surfaces)-2)]
-    
+
     run += 1
-    
-        
+
+
 data = [phase_num, candidate_num, mip_comp_gr, sl1m_comp_gr, fail1, fail2, failcase, THRESHOLD]
 
 
@@ -475,4 +476,30 @@ if SAVE:
     with open(fileName,'wb') as f:
         pickle.dump(data,f)
 
+from talos_rbprm.talos import Robot    as talosFull
+fb2 = talosFull()
+allfeetpos = res_L1.res[2]
 
+z_offset=0.1
+
+q_init = fb2.referenceConfig.copy()
+# ~ q_init[:3] = p_start
+q_end = q_init.copy()
+# ~ q_end [0:3] = p_goal
+# ~ q_end[-6:-3] = [0,0,0.]
+
+q_init[:7] = ps.configAtParam(pathId, 0.001)[:7]
+q_end [:7]= ps.configAtParam(pathId, ps.pathLength(pathId) - 0.001)[:7]
+q_end[2] += z_offset
+q_init[2] += z_offset
+
+from sl1m.constants_and_tools import replace_surfaces_with_ineq_in_problem
+pb = res_L1.pb
+del pb["phaseData"][0];
+replace_surfaces_with_ineq_in_problem(pb)
+
+from sl1m.sl1m_to_mcapi import build_cs_from_sl1m_mip
+cs = build_cs_from_sl1m_mip(pb, allfeetpos, fb2, q_init, q_end,z_offset=z_offset / 2)
+cs.saveAsBinary("talos_bridge.cs")
+
+v(q_init)
